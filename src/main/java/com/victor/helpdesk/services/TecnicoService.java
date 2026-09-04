bd7fcde8b90a269f0c1f209074f5462fb8983937
victor.scheuer.rodrigues@gmail.com <victor.scheuer.rodrigues@gmail.com>
2026-09-04 00:39:52 -0300

    exececao para objetos nao encontrados

--- src/main/java/com/victor/helpdesk/services/TecnicoService.java
+++ src/main/java/com/victor/helpdesk/services/TecnicoService.java
@@ -1,12 +1,22 @@
 package com.victor.helpdesk.services;
 
+
+import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.victor.helpdesk.domain.Pessoa;
 import com.victor.helpdesk.domain.Tecnico;
+import com.victor.helpdesk.domain.dtos.TecnicoDTO;
+import com.victor.helpdesk.repositories.PessoaRepository;
 import com.victor.helpdesk.repositories.TecnicoRepository;
+//import com.victor.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.victor.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -15,7 +25,7 @@ public class TecnicoService {
 	private TecnicoRepository repository;
 	
 	public Tecnico findById(Integer id) {
-		Optional<Tecnico> obj  = repository.findById(id);
-		return obj.orElse(null);
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id, null));
 	}
 }
