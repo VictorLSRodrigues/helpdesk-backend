231ac0a2050d3efdf8c23d3745bd40d405b0f681
victor.scheuer.rodrigues@gmail.com <victor.scheuer.rodrigues@gmail.com>
2026-09-04 23:58:09 -0300

    Validacao nos campos de criacao de novo usuario

--- src/main/java/com/victor/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/victor/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.victor.helpdesk.domain.Tecnico;
 import com.victor.helpdesk.domain.enums.Perfil;
@@ -15,11 +16,17 @@ public class TecnicoDTO  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	protected Integer id;
+	
+	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
 	
+	@NotNull(message = "O campo CPF é requerido")
 	protected String cpf;
 	
+	@NotNull(message = "O campo EMAIL é requerido")
 	protected String email;
+	
+	@NotNull(message = "O campo SENHA é requerido")
 	protected String senha;
 	
 	protected Set<Integer> perfis = new HashSet<>();
