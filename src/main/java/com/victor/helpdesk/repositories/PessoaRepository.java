cc61eaf99df4744172c2a0b25760780dd47f8b59
victor.scheuer.rodrigues@gmail.com <victor.scheuer.rodrigues@gmail.com>
2026-09-04 22:30:06 -0300

    exceção para cpf e email ja cadastrados

--- src/main/java/com/victor/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/victor/helpdesk/repositories/PessoaRepository.java
@@ -1,5 +1,7 @@
 package com.victor.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.yaml.snakeyaml.events.Event.ID;
 
@@ -7,5 +9,8 @@ import com.victor.helpdesk.domain.Pessoa;
 
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 
 }
