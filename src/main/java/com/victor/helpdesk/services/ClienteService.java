3dafd5d6ccd8f085b703de16ee1c0ed7fadc6553
victor.scheuer.rodrigues@gmail.com <victor.scheuer.rodrigues@gmail.com>
2026-09-07 19:27:35 -0300

    crud para cliente implementado

---
+++ src/main/java/com/victor/helpdesk/services/ClienteService.java
@@ -0,0 +1,77 @@
+package com.victor.helpdesk.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+//import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.victor.helpdesk.domain.Pessoa;
+import com.victor.helpdesk.domain.Cliente;
+import com.victor.helpdesk.domain.dtos.ClienteDTO;
+import com.victor.helpdesk.repositories.PessoaRepository;
+import com.victor.helpdesk.repositories.ClienteRepository;
+import com.victor.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.victor.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id, null));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj); 
+	}
+	
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		 Cliente oldObj  = findById(id);
+		 validaPorCpfEEmail(objDTO);
+		 oldObj = new Cliente(objDTO);
+		 return repository.save(oldObj);
+	}
+	
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		
+		if(obj.getChamados().size()> 0) {
+			throw new DataIntegrityViolationException("Cliente possui  ordens de seriviço e não pode ser delerado!");
+		}
+			repository.deleteById(id);
+	}
+	
+
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+	}
+
+}
