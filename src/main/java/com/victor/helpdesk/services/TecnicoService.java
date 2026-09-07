bca9a8d3c8c406446f85513397b625c96331bf02
victor.scheuer.rodrigues@gmail.com <victor.scheuer.rodrigues@gmail.com>
2026-09-07 17:59:56 -0300

    metodo put para tecnico implementado e debug do pom.xml

--- src/main/java/com/victor/helpdesk/services/TecnicoService.java
+++ src/main/java/com/victor/helpdesk/services/TecnicoService.java
@@ -4,6 +4,8 @@ package com.victor.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 //import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +43,14 @@ public class TecnicoService {
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj); 
 	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		 Tecnico oldObj  = findById(id);
+		 validaPorCpfEEmail(objDTO);
+		 oldObj = new Tecnico(objDTO);
+		 return repository.save(oldObj);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -53,5 +63,6 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
+
 	
 }
