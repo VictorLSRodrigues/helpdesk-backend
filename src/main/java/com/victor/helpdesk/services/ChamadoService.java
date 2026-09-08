7e6865e8b1b2964eb4703ee474b1be6d2f9d6f4f
victor.scheuer.rodrigues@gmail.com <victor.scheuer.rodrigues@gmail.com>
2026-09-08 01:13:57 -0300

    metodo find by all chamados

--- src/main/java/com/victor/helpdesk/services/ChamadoService.java
+++ src/main/java/com/victor/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.victor.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,9 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 }
